27ea531897c3d044f2465c11929f2dac8410dc7b
Unknown <andrewpetersell@gmail.com>
2017-02-18 13:42:03 -0500

    GhostMovement

---
+++ Assets/Scripts/GhostControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostControls : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private SpriteRenderer sr;
+	public float speed;
+	public int playerNum;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+		Move ();
+	}
+
+	void Move(){
+		Vector2 direction = new Vector2 (Input.GetAxis ("Horizontal_P" + playerNum), Input.GetAxis ("Vertical_P" + playerNum));
+
+		rb.velocity = speed * direction;
+
+		if (direction.x >= 0) {
+			sr.flipX = true;
+		} else {
+			sr.flipX = false;
+		}
+	}
+}
