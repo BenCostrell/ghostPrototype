0b0bb6ff169ed9d60aebcc936f2c77f9975cf83d
Unknown <andrewpetersell@gmail.com>
2017-02-18 11:53:25 -0500

    New Scene + Sprites Folder

---
+++ Assets/Scripts/ObjectProperties.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectProperties : MonoBehaviour {
+
+	public bool isBlue;
+	public bool isPink;
+	public bool laptop;
+	public bool lamp;
+	public bool book;
+	public bool palette;
+	public bool synth;
+	public bool teddy;
+
+	public Sprite laptopSprite;
+	public Sprite lampSprite;
+	public Sprite bookSprite;
+	public Sprite paletteSprite;
+	public Sprite synthSprite;
+	public Sprite teddySprite;
+
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
