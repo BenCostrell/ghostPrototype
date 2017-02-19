6a8bb7c8556e9e39984fadb695f79b4b560c1111
Unknown <andrewpetersell@gmail.com>
2017-02-18 19:58:31 -0500

    Updated Object Manager Prefab

---
+++ Assets/Scripts/TweetManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweetManager : MonoBehaviour {
+
+	public Vector2 tweetField1;
+	public Vector2 tweetField2;
+	public Vector2 tweetField3;
+	public Vector2 tweetField4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
