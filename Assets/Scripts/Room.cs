69c0fd6fa4c0159a42ba41ecf771c246cabd1fd9
Unknown <andrewpetersell@gmail.com>
2017-02-18 20:53:20 -0500

    Room Color Changer

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -18,22 +18,24 @@ public class Room : MonoBehaviour {
 	public int ownerNum;
 	public float fillProportion;
 	public float fillRate;
+	SpriteRenderer sr;
+	GameObject colorManager;
+	RoomColorManager rcm;
 
-	public Color blueDark; 
-	public Color blueBright; 
-	public Color blueLerp; 
-	public Color pinkDark; 
-	public Color pinkBright;
-	public Color pinkLerp; 
+
 
 	// Use this for initialization
 	void Start () {
 		ownerNum = 0;
 		fillProportion = 0;
+		sr = GetComponent<SpriteRenderer> ();
+
+		colorManager = GameObject.Find ("RoomColorManager");
+		rcm = colorManager.GetComponent<RoomColorManager> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () { 
 
 		spoopEnder ();
 		
@@ -78,8 +80,24 @@ public class Room : MonoBehaviour {
 
 	public void IncrementFillProportion(){
 		fillProportion += fillRate;
+		if (ownerNum == 1) 
+		{
+			sr.color = rcm.blueLerp;
+		}
+		if (ownerNum == 2) 
+		{
+			sr.color = rcm.pinkLerp;
+		}
 		if (fillProportion >= 1) {
 			Debug.Log ("finished claiming for player " + ownerNum);
+			if (ownerNum == 1) 
+			{
+				sr.color = rcm.blueBright;
+			}
+			if (ownerNum == 2) 
+			{
+				sr.color = rcm.pinkBright;
+			}
 		}
 	}
 
